a3e0fcfbbaae8f338f40fe3d785648b3150c91d3
teiji0926 <118594404+teiji0926@users.noreply.github.com>
2023-12-03 12:06:08 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import openai
+openai_api_key = st.secrets.OpenAIAPI.openai_api_key
+client = openai.OpenAI(api_key=openai_api_key)
+
+if 'messages' not in st.session_state:
+  st.session_state['messages'] = [
+      {'role':'system','content':'あなたは可愛くてセクシーなアシスタントです。'}
+  ]
+
+def communicate():
+  messages = st.session_state['messages']
+  user_message = {'role':'user','content':st.session_state['user_input']}
+  messages.append(user_message)
+
+  response = client.chat.completions.create(
+      model = 'gpt-3.5-turbo',
+      messages = messages,
+      temperature = st.session_state['temperature']
+  )
+
+  bot_message = {'role':'system','content':response.choices[0].message.content}
+  messages.append(bot_message)
+
+  st.session_state["user_input"] = ""  # 入力欄を消去
+
+
+st.set_page_config(page_title='My App',  # アプリのタイトル
+                   page_icon='👙')       # 絵文字またはファビコンのファイルパス
+
+st.session_state['temperature'] = st.sidebar.slider('AIの自由度',0.9,1.1,1)
+user_input = st.text_input('なんでも話してください',key='user_input',on_change=communicate)
+
+st.write('## ChatGptと会話しよう')
+
+if st.session_state['messages']:
+  messages = st.session_state['messages']
+
+  for message in reversed(messages):
+    speaker = '😒'
+    if message['role'] == 'system':
+      speaker = '💖'
+    
+    st.write(speaker + ":" + message["content"])
